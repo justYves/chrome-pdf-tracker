03aac97cdd5a5a88876785a150061227d28289e9
Yves Yuen <yves.j.yuen@gmail.com>
2015-09-02 16:54:46 -0400

    created basic manifest

---
+++ background.js
@@ -0,0 +1,10 @@
+chrome.app.runtime.onLaunched.addListener(function() {
+
+  alert("it works!");
+  // chrome.app.window.create('window.html', {
+  //   'outerBounds': {
+  //     'width': 400,
+  //     'height': 500
+  //   }
+  // });
+});
